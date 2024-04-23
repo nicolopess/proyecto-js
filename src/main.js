33e920bdb7699fd99ddb6d3ade0948ee36e80cbc
Nicolas Lopes <nilopes97@gmail.com>
2024-04-22 22:29:21 -0300

    Correccion

--- src/main.js
+++ src/main.js
@@ -5,35 +5,27 @@ const playerdisplay = document.getElementById("playerdisplay");
 const robotdisplay = document.getElementById("robotdisplay");
 const resultadodisplay = document.getElementById("resultadodisplay");
 
-function playgame(jugadoropcion){
+function playgame(jugadoropcion) {
+  const robotopcion = opciones[Math.floor(Math.random() * 3)];
+  let result = "";
 
-const robotopcion = opciones[Math.floor(Math.random() * 3)];
-let result = "";
+  if (jugadoropcion === robotopcion) {
+    result = "ES UN EMPATE";
+  } else {
+    switch (jugadoropcion) {
+      case "piedra":
+        result = robotopcion === "tijera" ? "GANASTE!!" : "HAS PERDIDO";
+        break;
+      case "papel":
+        result = robotopcion === "piedra" ? "GANASTE!!" : "HAS PERDIDO";
+        break;
+      case "tijera":
+        result = robotopcion === "papel" ? "GANASTE!!" : "HAS PERDIDO";
+        break;
+    }
+  }
 
-if(jugadoropcion === robotopcion){
-result = "ES UN EMPATE";
+  playerdisplay.textContent = `Tú: ${jugadoropcion}`;
+  robotdisplay.textContent = `Mr. Robot: ${robotopcion}`;
+  resultadodisplay.textContent = result;
 }
-
-else{
-
-switch(jugadoropcion){
-    case "piedra":
-    result = (robotopcion === "tijera") ? "GANASTE!!" : "HAS PERDIDO";
-    break;
-    case "papel":
-    result = (robotopcion === "piedra") ? "GANASTE!!" : "HAS PERDIDO";
-    break;
-    case "tijera":
-    result = (robotopcion === "papel") ? "GANASTE!!" : "HAS PERDIDO";
-    break;
-    
-}
-
-}
-
-playerdisplay.textContent = jugadoropcion;
-robotdisplay.textContent = robotopcion;
-resultadodisplay.textContent = result;
-
-}
-
